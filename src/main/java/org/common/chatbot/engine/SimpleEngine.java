9da819012766debf7303e6505b00192eafe5df83
Ayush vipul <ayush.vipul@outlook.com>
2020-01-29 06:57:41 +0530

    With Sping and IOC

--- src/main/java/org/common/chatbot/engine/SimpleEngine.java
+++ src/main/java/org/common/chatbot/engine/SimpleEngine.java
@@ -1,5 +1,8 @@
 package org.common.chatbot.engine;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public interface SimpleEngine {
 
     public String respondToMessage(String message);
